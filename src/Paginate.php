124ae83d33b77c293c7344fec38604f5094673ee
Amir Fayaz <a.fayazbakhsh1@gmail.com>
2023-10-11 17:53:33 +0330

    Create main painate class

---
+++ src/Paginate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Amir\PaginateLimit;
+
+use Illuminate\Support\Collection;
+
+class Paginate
+{
+    public function my(?Collection $collection = null)
+    {
+        return $collection;
+    }
+}
