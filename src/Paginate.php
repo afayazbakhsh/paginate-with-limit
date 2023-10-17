92c818e0bbd1e49e4eaf7615e321f178aaba755d
Amir Fayaz <a.fayazbakhsh1@gmail.com>
2023-10-17 12:47:32 +0330

    : invokable method to handle paginate with collection macro

--- src/Paginate.php
+++ src/Paginate.php
@@ -1,13 +1,26 @@
 <?php
 
-namespace Amir\PaginateLimit;
+namespace Amir\PaginateCollection;
 
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
 class Paginate
 {
-    public function my(?Collection $collection = null)
-    {
-        return $collection;
-    }
+  public function __invoke()
+      {
+          Collection::macro('paginate', function($pageName = 'page', int $perPage = 15, int $total = null, $page = null){
+              $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+              return new LengthAwarePaginator(
+                  $this->forPage($page, $perPage),
+                  $total ?: $this->count(),
+                  $perPage,
+                  $page,
+                  [
+                      'path' => LengthAwarePaginator::resolveCurrentPath(),
+                      'pageName' => $pageName,
+                  ]
+                );
+          });
+      }
 }
